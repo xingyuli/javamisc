a45878d37f79d4957d60f1b1fad042031e8a973e
Liu Xingyu <xingyulliiuu@gmail.com>
2013-11-22 16:07:31 +0800

    Play with the FileChannel.

---
+++ src/main/java/org/swordess/javamisc/nio/FileChannelTest.java
@@ -0,0 +1,95 @@
+package org.swordess.javamisc.nio;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class FileChannelTest {
+
+	@Test
+	public void testRead() {
+		FileInputStream inStream = null;
+		
+		try {
+			String path = FileChannelTest.class.getResource("data/nio-data-read.txt").getPath();
+			inStream = new FileInputStream(path);
+			
+			FileChannel inChannel = inStream.getChannel();
+			
+			// allocate the bytes on the Heap
+			ByteBuffer buf = ByteBuffer.allocate(256);
+			byte[] dst = new byte[256];
+			
+			byte zero = 0;
+			int bytesRead = -1;
+			while (-1 != (bytesRead = inChannel.read(buf))) {
+				buf.position(0);
+				buf.get(dst);
+				buf.clear();
+				
+				String text = new String(dst, 0, bytesRead);
+				Arrays.fill(dst, zero);
+				
+				System.out.print(text);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (null != inStream) {
+				try {
+					inStream.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void testWrite() {
+		String path = FileChannelTest.class.getResource("").getPath() + "data/nio-data-write.txt";
+		File f = new File(path);
+		if (f.exists()) {
+			f.delete();
+		}
+		
+		try {
+			f.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		FileOutputStream outStream = null;
+		try {
+			outStream = new FileOutputStream(path);
+			FileChannel outChannel = outStream.getChannel();
+			
+			ByteBuffer buf = ByteBuffer.wrap("This is nothing special!".getBytes());
+			while (buf.hasRemaining()) {
+				outChannel.write(buf);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (null != outStream) {
+				try {
+					outStream.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+	
+}
