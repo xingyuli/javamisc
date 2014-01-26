437308a4c89ea478454acc5e9bcf6bc199b6f060
Liu Xingyu <xingyulliiuu@gmail.com>
2014-01-26 18:05:43 +0800

    eXtremme Programming - Test Cases

---
+++ src/main/java/org/swordess/javamisc/xp/CheckForPrime.java
@@ -0,0 +1,14 @@
+package org.swordess.javamisc.xp;
+
+public class CheckForPrime {
+
+	public boolean primeCheck(int i) {
+		// TODO
+		return false;
+	}
+
+	public void checkArgs(String[] args) {
+		// TODO
+	}
+
+}
