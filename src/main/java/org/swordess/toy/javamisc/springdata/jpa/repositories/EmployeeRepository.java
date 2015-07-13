620d8f63d7383e6687d3b0ac7c2c866d7e2e14df
Vic Lau <xingyulliiuu@gmail.com>
2015-07-13 14:46:41 +0800

    integrate with hibernate modelgen

--- src/main/java/org/swordess/toy/javamisc/springdata/jpa/repositories/EmployeeRepository.java
+++ src/main/java/org/swordess/toy/javamisc/springdata/jpa/repositories/EmployeeRepository.java
@@ -4,13 +4,14 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.swordess.toy.javamisc.springdata.jpa.model.Employee;
+import org.swordess.toy.javamisc.springdata.jpa.model.Employee_;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 
     class Specs {
 
         public static Specification<Employee> olderThan(int age) {
-            return (r, q, b) -> b.gt(r.get("age"), age);
+            return (root, query, cb) -> cb.gt(root.get(Employee_.age), age);
         }
 
     }
